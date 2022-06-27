c48679a72bc78a2d50811f8b727523dd28b2f6ba
Bradford Knight <bradford.a.knight@gmail.com>
2022-06-27 18:29:28 -0400

    Add Shuffle Function

--- deck.js
+++ deck.js
@@ -1,9 +1,22 @@
 const Suits = ["Spade", "Clover", "Heart", "Diamond"]
 const Values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
-class Deck {
-    constructor(cards){
+export default class Deck {
+    constructor(cards = newDeck()){
         this.cards = cards
+    }
+
+    get numberOfCards(){
+        this.cards.length
+    }
+
+    shuffle(){
+      for (let i = this.numberOfCards - 1; i > 0; i--){
+        const newPosition = Math.floor(Math.random() * (i + 1))
+        const oldPosition = this.cards[newPosition]
+        this.cards[newPosition] = this.card[i]
+        this.card[i] = oldPosition
+      }  
     }
 }
 
