8d64f642fa510db5cf43129d64e08458d4edeabd
Bradford Knight <bradford.a.knight@gmail.com>
2022-06-29 20:27:04 -0400

    Added Dealer hand functionality, added card value parsing function, added Ace check function, added game start-up function

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ let canHit = true; //player can draw while sum < 21
 
 window.onload = newDeck();
 window.onload = shuffleDeck();
+window.onload = startGame();
 
 function newDeck() {
     let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
@@ -35,11 +36,45 @@ function shuffleDeck(){
         deck[i] = deck[j];  //**Need to understand how this line works** Swapping the i index for randomized j index is occuring, but how specifically does it work?
         deck[j] = temp;
     }
-    console.log(deck);
+    console.log('shuffled deck:', deck);
 }
 
-// import Deck from "./deck.js"
+function startGame(){
+    hidden = deck.pop();
+    dealerSum += getValue(hidden);
+    dealerAceCount += checkAce(hidden)
+    // console.log(hidden)
+    // console.log(dealerSum)
+    while (dealerSum < 16) {
+        let cardImg = document.createElement("img")
+        let card = deck.pop();
+        cardImg.src = "./cards/" + card + ".png"
+        dealerSum += getValue(card)
+        dealerAceCount += checkAce(card);
+        document.getElementById("dealer-hand").append(cardImg);
+    }
+    console.log('dealer sum:', dealerSum)
+}
 
-// // const deck = new Deck()
-// // deck.shuffle()
-// // console.log(deck.cards)
+function getValue(card){
+    let cardData = card.split("-"); //Provides array with values left & right of the dash
+    let cardValue = cardData[0] // selects the value left of the dash, array position [0]
+
+    if (isNaN(cardValue)){
+        if (cardValue === "A"){
+            return 11;
+        }else{
+            return 10;
+        }
+    }else{ 
+        return parseInt(cardValue)
+    }
+}
+
+function checkAce(card){
+    if (card[0] == "A"){
+        return 1;
+    }else {
+        return 0;
+    }
+}
